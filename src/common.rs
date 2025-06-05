2d2d09ce2830a370b08364a04813055a4b4e4afa
DA LUO <152002890+luoda2023@users.noreply.github.com>
2025-06-06 00:31:19 +0800

    Update common.rs

--- src/common.rs
+++ src/common.rs
@@ -996,7 +996,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "http://rustdeskapi.dicad.cn:21114".to_owned()
+    "http://rustdesk.dicad.cn:21114".to_owned()
 }
 
 #[inline]
