bf4c895548e6b5d881b026f9622a05fa0f4f177e
DA LUO <152002890+luoda2023@users.noreply.github.com>
2025-06-05 14:35:05 +0800

    Update cn.rs

--- src/lang/cn.rs
+++ src/lang/cn.rs
@@ -145,7 +145,7 @@ pub static ref T: std::collections::HashMap<&'static str, &'static str> =
         ("Failed to make direct connection to remote desktop", "无法直接连接到远程桌面"),
         ("Set Password", "设置密码"),
         ("OS Password", "操作系统密码"),
-        ("install_tip", "你正在运行未安装版本，由于 UAC 限制，作为被控端，会在某些情况下无法控制鼠标键盘，或者录制屏幕，请点击下面的按钮将 RustDesk 安装到系统，从而规避上述问题。"),
+        ("install_tip", "欢迎浏览器输入以上Dicad.cn网址，免费畅玩全部解压游戏！友情提醒：你正在受限运行软件未安装版，请点击下面的“安装”按钮将软件安装到系统，即可使用全部功能版本。"),"),
         ("Click to upgrade", "点击这里升级"),
         ("Click to download", "点击这里下载"),
         ("Click to update", "点击这里更新"),
