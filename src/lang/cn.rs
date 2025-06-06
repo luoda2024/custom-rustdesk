220f98e8a704c70469383880f1578b5f08d21005
DA LUO <152002890+luoda2023@users.noreply.github.com>
2025-06-06 14:56:26 +0800

    Update cn.rs

--- src/lang/cn.rs
+++ src/lang/cn.rs
@@ -13,7 +13,7 @@ pub static ref T: std::collections::HashMap<&'static str, &'static str> =
         ("Service is running", "服务正在运行"),
         ("Service is not running", "服务未运行"),
         ("not_ready_status", "未就绪，请检查网络连接"),
-        ("Control Remote Desktop", "✍️填入对方远程软件ID"),
+        ("Control Remote Desktop", "填写对方远程ID"),
         ("Transfer file", "传输文件"),
         ("Connect", "远程连接"),
         ("Recent sessions", "最近访问过"),
